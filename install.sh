a0d32b7ffe70d4efd3da08744a661841e3175727
SamahaAbbas <127323854+SamahaAbbas@users.noreply.github.com>
2024-10-05 17:43:44 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ elif command -v yum >/dev/null; then
 yum update -y
 yum install gcc-c++ libpcap-devel.x86_64 libpcap.x86_64 "ncurses*"
 fi
-sudo wget -O /root/nethogs.zip https://github.com/HamedAp/Nethogs-Json/archive/refs/heads/main.zip
+sudo wget -O /root/nethogs.zip https://github.com/SamahaAbbas/Nethogs-Json/archive/refs/heads/main.zip
 sudo unzip /root/nethogs.zip
 sudo mv -f /root/Nethogs-Json-main /root/nethogs
 cd /root/nethogs/
